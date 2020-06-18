d00b988f7f7fd8a9cb742f2288c1aa363a7a86d8
ogarmela <52357639+ogarmela@users.noreply.github.com>
2020-06-17 19:31:45 -0700

    Add files via upload

---
+++ muna.java
@@ -0,0 +1,26 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class muna {
+	public void stutter(Stack<Integer> s) {
+	    Queue<Integer> q = new LinkedList<Integer>();
+	        
+	    while(!s.isEmpty())
+	        q.add(s.pop());
+	                        
+	    while(!q.isEmpty())
+	        s.push(q.remove());
+	                                        
+	    while(!s.isEmpty())
+	        q.add(s.pop());
+	                                                        
+	    while(!q.isEmpty()) {
+	        int n = q.remove();
+	        s.push(n);
+	        s.push(n);
+	    }
+	}
+
+
+}
